1d89c7b268b0e6837bb30d646a62cb67526b6029
Peter Tait <contact@petertait.com>
2017-04-12 16:16:19 +0100

    initial

--- craft/config/general.php
+++ craft/config/general.php
@@ -1,36 +1,27 @@
 <?php
-
 /**
  * General Configuration
  *
- * All of your system's general configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the default settings in `vendor/craftcms/cms/src/config/defaults/general.php`.
  */
 
-return array(
-    '*' => array(
-      'omitScriptNameInUrls' => true,
-    ),
-    'craft-react.dev' => array(
-        // ...
-        'environmentVariables' => array(
-            'basePath' => '/users/sites/craft-react.com/public/',
-            'baseUrl'  => 'http://craft-react.dev/',
-            'siteUrl' => 'http://craft-react.dev/',
-        ),
+return [
+    // Base site URL
+    'siteUrl' => null,
 
-    ),
+    // Default Week Start Day (0 = Sunday, 1 = Monday...)
+    'defaultWeekStartDay' => 0,
 
-    'craft-react.com' => array(
-        // ...
+    // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+    'enableCsrfProtection' => true,
 
-        'environmentVariables' => array(
-            'basePath' => '/public_html/',
-            'baseUrl'  => 'http://craft-react.com/',
-        ),
-      'siteUrl' => array(
-          'en' => 'http://craft-react.com/',
-      ),
-    )
+    // Whether "index.php" should be visible in URLs (true, false, "auto")
+    'omitScriptNameInUrls' => true,
 
-);
+    // Control Panel trigger word
+    'cpTrigger' => 'admin',
+
+    // Dev Mode (see https://craftcms.com/support/dev-mode)
+    'devMode' => false,
+];
